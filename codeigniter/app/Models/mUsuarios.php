6d605eea35294e0faee17fd806120fdc7e8a931f
Andrea2-Mich <michelalfarog21@gmail.com>
2022-02-25 11:26:15 -0600

    editar codigos de mUsuarios
    correccion de errores, modelo completo

--- codeigniter/app/Models/mUsuarios.php
+++ codeigniter/app/Models/mUsuarios.php
@@ -3,6 +3,7 @@ namespace App\Models;
 use CodeIgniter\Model;
 
 class mUsuarios extends Model {
+    
     protected $table = 'usuario';
     protected $primaryKey = 'id_usuario';
 
@@ -12,6 +13,15 @@ class mUsuarios extends Model {
     protected $useSoftDeletes = true;
 
     protected $allowedFields = ['usuario', 'password'];
+
+    protected $useTimestamps = false;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
 
 }
 
