51b94708dc76759ce4b6db48459be2feccbe3725
Garkarius <84213528+Garkarius@users.noreply.github.com>
2022-03-04 09:32:01 -0600

    archivos ingreso creado

---
+++ codeigniter/app/Views/vIngreso.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Ingreso</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Ingreso de usuario registrado</h1>
+        <form action="<?php echo base_url();?>/Home/ingresarForm" method="POST">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" placeholder="email@example.com">
+            <br><br>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+            <button type="submit">Ingresar</button>
+        </form>
+    </div>
+</body>
+
+</html>
