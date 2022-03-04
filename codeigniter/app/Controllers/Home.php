c7ac125e23f98f3a9368efaa37b012f37eaa17fd
Garkarius <84213528+Garkarius@users.noreply.github.com>
2022-03-04 09:04:06 -0600

    controlador Home actualizado

--- codeigniter/app/Controllers/Home.php
+++ codeigniter/app/Controllers/Home.php
@@ -32,9 +32,37 @@ class Home extends BaseController
         return view("vSuccess", $datoId);
     }
 
+    public function mostrarRegistros(){
+        $mUsuarios = new mUsuarios();
+        $todos = $mUsuarios->findAll();
+        $usuarios = array('usuarios'=>$todos);
+
+        return view("vRegistros",$usuarios);
+    }
+
+    public function ingresarForm(){
+        $mUsuarios = new mUsuarios();
+        $usuario = $_POST['email'];
+        $password = $_POST['password'];
+        $user = $mUsuarios->where('usuario', $usuario)->where('password', $password)->first();
+        
+        return view("vIngresado",$user);
+    }
+
     public function buscarRegistro(){
         $mUsuarios = new mUsuarios();
-        
+        $i_usuario = $_POST['id_usuario'];
+        $usuario = $mUsuarios -> find($id_usuario);
+        return view("vRegistroEncontrado", $usuario);   
+    }
+
+    public function actualizarRegistro(){
+        $mUsuarios = new musuarios();
+        $id_usuario = $_POST['id_usuario'];
+        $usuarioActualizado = ["usuario" => $_POST['email'], "password" =>$_POST['password']];
+        $mUsuarios -> update($id_usuario, $usuarioActualizado);
+
+        return $this -> mostrarRegistros();
     }
 
 }
