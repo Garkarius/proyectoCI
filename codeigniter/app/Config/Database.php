92fe50545e32fc3c5a1d526ff60ea35160f2b642
Garkarius <84213528+Garkarius@users.noreply.github.com>
2022-03-08 22:02:35 -0600

    cambio de nombre de la base de datos

--- codeigniter/app/Config/Database.php
+++ codeigniter/app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'modelo',
+        'database' => 'modal',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
